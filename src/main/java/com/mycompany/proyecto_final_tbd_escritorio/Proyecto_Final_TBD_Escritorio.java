9575f6231d3bb25e6b6301c7755b670496d29ce3
eduar <eduardo.dc@tecjerez.edu.mx>
2025-11-12 17:39:02 -0600

    First commit

---
+++ src/main/java/com/mycompany/proyecto_final_tbd_escritorio/Proyecto_Final_TBD_Escritorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_final_tbd_escritorio;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class Proyecto_Final_TBD_Escritorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
