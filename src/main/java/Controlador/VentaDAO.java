1042b2edb415a830c797a6089a2f9e77158bb4db
eduar <eduardo.dc@tecjerez.edu.mx>
2025-11-22 10:55:08 -0600

    Se corrigio un error

--- src/main/java/Controlador/VentaDAO.java
+++ src/main/java/Controlador/VentaDAO.java
@@ -4,10 +4,60 @@
  */
 package Controlador;
 
+import Conexion.ConexionBD;
+
+import Modelo.Venta;
+
 /**
  *
  * @author Eduardo
  */
 public class VentaDAO {
+    
+    ConexionBD con = ConexionBD.getInstancia();
+    
+    public boolean agregarVenta(Venta venta){
+        
+        String sql = "INSERT INTO ventas(fecha_venta, precio_final, forma_pago, id_cliente, id_empleado, id_vehiculo) VALUES(?,?,?,?,?,?);";
+        
+        return con.ejecutarInstruccionLMD(sql,
+                
+            venta.getFechaVenta(),
+            venta.getPrecioFinal(),
+            venta.getFormaPago(),
+            venta.getIdCliente(),
+            venta.getIdEmpleado(),
+            venta.getIdVehiculo()
+                
+        );
+        
+    }
+    
+    public boolean eliminarVenta(int id_venta){
+        
+        String sql = "DELETE FROM ventas WHERE id_venta = ?";
+        
+        return con.ejecutarInstruccionLMD(sql, id_venta);
+        
+    }
+    
+    public boolean actualizarVenta(Venta venta, int id_venta){
+        
+        String sql = "UPDATE ventas SET fecha_venta = ?, precio_final = ?, forma_pago = ?, id_cliente = ?, id_empleado = ?, id_vehiculo = ? WHERE id_venta = ?";
+        
+        return con.ejecutarInstruccionLMD(sql, 
+          
+            venta.getFechaVenta(),
+            venta.getPrecioFinal(),
+            venta.getFormaPago(),
+            venta.getIdCliente(),
+            venta.getIdEmpleado(),
+            venta.getIdVehiculo(),
+            id_venta
+                
+        );
+        
+    }
+    
     
 }
